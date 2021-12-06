fbbe0fa3edea9181658c83cb4d83738a981e1cec
ArunSingh <arunsingh.vellore@gmail.com>
2021-12-06 10:30:16 +0530

    You are doing great

---
+++ maps.go
@@ -0,0 +1,30 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	//composite literal
+	m := map[string]int{
+		"arun":  23,
+		"singh": 35,
+	}
+	//fmt.Printf("%v%T", m, m)
+
+	//,ok idiom to check element is present of not
+	if _, ok := m["arun"]; ok {
+		fmt.Println("Hi is present")
+	} else {
+		fmt.Println("Absent")
+	}
+
+	delete(m, "arun")
+
+	if _, ok := m["arun"]; ok {
+		fmt.Println("Hi is present")
+	} else {
+		fmt.Println("Absent")
+	}
+}
